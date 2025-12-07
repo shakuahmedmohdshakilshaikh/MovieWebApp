c1a5bd1f9bdd5ceb44dd91f8db889d4388c5159f
shakuahmedmohdshakilshaikh <121711069+shakuahmedmohdshakilshaikh@users.noreply.github.com>
2025-12-08 01:08:24 +0530

    Change Authorization header to user prompt
    Updated the Authorization header to prompt for user access token.

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
      headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMWIzMGMwOGI1OGE1YjRiNTE2NzBmOWYwNjY3MDU1MCIsIm5iZiI6MTc2NDM0ODA3Ni41NDMsInN1YiI6IjY5MjlkMGFjZmVmYjcwNTAxNDI2YzkzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8Gsi47P2EDOvfBmXcQFc9jp-0XtAgX7R-JNAa25Y6eA'
+    Authorization: 'give your access token'
   }
 });
 
